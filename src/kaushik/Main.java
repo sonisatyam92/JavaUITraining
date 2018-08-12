49b7b47fb9da6fa7189bba80c4b1e4dd9e155115
kaushikc1 <42308825+kaushikc1@users.noreply.github.com>
2018-08-11 20:57:46 -0700

    Update Main.java

--- src/kaushik/Main.java
+++ src/kaushik/Main.java
@@ -2,6 +2,18 @@ package kaushik;
 
 public class Main {
     public static void main(String[] args) {
+        int a=2;
+        int b=6;
+        if (a>b) {
+            System.out.println(a * b);
+        }
+        if (a<b) {
+            System.out.println(b/a);
+
+        }
+            if(a==b){
+                System.out.println(a>>b);
+            }
 
     }
 }
