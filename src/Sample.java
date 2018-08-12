ec34978ae75ca178bd23a677ba9bf267f27814a8
sonisatyam92 <sonisatyam92@gmail.com>
2018-08-12 08:46:22 +0530

    Session 2

---
+++ src/Sample.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        int a = scanner.nextInt();
+        System.out.println("Please enter second number");
+        int b = scanner.nextInt();
+        System.out.println("Please enter the operator u wanna use : " +
+                " [1-Add, 2-Substract , 3-Multiply, 4- Divide]");
+
+        int op =  scanner.nextInt();
+        if(op == 1){
+            System.out.println(a+b);
+        }else if(op == 2 ){
+            System.out.println(a-b);
+        }else if(op==3){
+            System.out.println(a*b);
+        }else if(op==4){
+            System.out.println(a/b);
+        } else {
+            System.out.println("Invalid operator");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
