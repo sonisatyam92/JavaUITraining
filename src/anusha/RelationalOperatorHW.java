b435be5a730878a87fa0f4a086116e957dfe9d83
ashaik1923 <42287242+ashaik1923@users.noreply.github.com>
2018-08-12 02:34:22 -0400

    RelationalOperatorsHW

---
+++ src/anusha/RelationalOperatorHW.java
@@ -0,0 +1,15 @@
+package anusha;
+
+public class RelationalOperatorHW {
+    public static void main(String[] args) {
+    	int a=12;
+    	int b=24;
+    	
+    	if(a>b)
+    		System.out.println("as a is greater than b, performing multiplication a*b----"+ a*b);
+    	if(a<b)
+    		System.out.println("as b is greater than a, performing division b/a----"+ b/a);
+    	if(a==b)
+    		System.out.println("as a and b are equal we perform bit shift a>>b----"+ (a>>b));
+    }
+}
