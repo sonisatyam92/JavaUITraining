e1157712d7a0315dd4f540af7d2426d9f382f764
charankandukuri <42285707+charankandukuri@users.noreply.github.com>
2018-08-12 22:03:37 -0400

    Update Main.java

--- src/charan/Main.java
+++ src/charan/Main.java
@@ -2,6 +2,16 @@ package charan;
 
 public class Main {
     public static void main(String[] args) {
+         int a = 10;
+        int b = 20;
 
+        if (a>b){
+            System.out.println (a*b);
+        } else if (a<b){
+            System.out.println (b/a);
+
+        }else if (a==b){
+            System.out.println (a>>b);
+        }
     }
 }
